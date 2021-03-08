fe14dd418f6074982ad0288851f4d2aba2b1b327
Sean <seangullery@gmail.com>
2021-03-08 20:34:32 +1300

    Player Detection

---
+++ Thief Game/Assets/Scripts/Sean Scripts/CameraDetection.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDetection : MonoBehaviour
+{
+
+    private float Timer;
+    private bool timerStarter;
+    
+    // Update is called once per frame
+    void Update()
+    {
+       
+      
+       
+       if(timerStarter == true)
+       {
+            Timer += Time.deltaTime;     
+       }
+       else if(timerStarter == false)
+       {
+           Timer = 0;
+           Debug.Log("You have been Reset");
+       }
+
+        if(Timer >= 2)
+        {
+            Debug.Log("You have been caught");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            timerStarter = true;
+        }
+        else
+        {
+            timerStarter = false;
+        }   
+    }
+
+  
+
+
+}
