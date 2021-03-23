87d4271b7a9e9bb2e4c50f604785b4e280976e03
Peet <peetvipz@gmail.com>
2021-03-23 18:56:22 +1300

    Update

--- Thief Game/Assets/Scripts/PeetScripts/AudioManager.cs
+++ Thief Game/Assets/Scripts/PeetScripts/AudioManager.cs
@@ -6,18 +6,11 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
 
-    public static AudioManager instance;
+    
     // Start is called before the first frame update
     void Awake()
     {
-        if (instance == null)
-            instance = this;
-        else 
-        {
-            Destroy(gameObject);
-            return;
-        }
-        DontDestroyOnLoad(gameObject);
+        
 
         foreach (Sound s in sounds)
         {
