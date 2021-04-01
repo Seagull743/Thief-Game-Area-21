38f6ef5be8ba7ce471c3ab3fa5fcc1be45bbf663
Sean <seangullery@gmail.com>
2021-04-01 13:49:06 +1300

    Full Game Build

--- Thief Game/Assets/Scripts/Sean Scripts/LightBoxInteract.cs
+++ Thief Game/Assets/Scripts/Sean Scripts/LightBoxInteract.cs
@@ -10,12 +10,19 @@ public class LightBoxInteract : InteractiveObject
     public GM gm;
 
     public GameObject[] bluewire;
+    public GameObject[] blackwire;
  
 
     void Start()
     {
         anim.GetComponent<Animator>();
-        
+
+        foreach (GameObject g in blackwire)
+        {
+            g.SetActive(false);
+        }
+
+
         foreach (GameObject g in bluewire)
         {
             g.SetActive(true);
@@ -31,10 +38,16 @@ public class LightBoxInteract : InteractiveObject
         LB.GetComponent<LightBox>().TurnOffBox();
         gm.GetComponent<GM>().Normalcross();
 
+        foreach (GameObject g in blackwire)
+        {
+            g.SetActive(true);
+        }
+
         foreach (GameObject g in bluewire)
         {
             g.SetActive(false);
         }
+
 
     }
 
